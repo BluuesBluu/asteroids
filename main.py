989c973ce6d754b3078e498ed280ea6df92e971d
BluuesBluu <maciejczak@dotbluu.eu>
2024-08-24 00:57:46 +0200

    env setup

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        pygame.fill((0,0,255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
 
 
